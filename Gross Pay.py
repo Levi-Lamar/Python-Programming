e178ba741d7b05f2ca6151bc51caf35bd0ba1d85
Levi-Lamar <97467036+Levi-Lamar@users.noreply.github.com>
2022-01-28 16:47:16 +0300

    Add files via upload

---
+++ Gross Pay.py
@@ -0,0 +1,5 @@
+#Gross Pay
+Hours = int(input("Enter Hours: "))
+Rate = float(input("Enter Rate: "))
+GrossPay = (Hours*Rate)
+print("Pay: ", GrossPay)
---
+++ Numerical Expressions.py
@@ -0,0 +1,9 @@
+#Numerical Expressions
+a = 21
+b = 6
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a*b=",a*b)
+print("a/b=",a/b)
+print("a**b=",a**b)
+print("a%b=",a%b)
