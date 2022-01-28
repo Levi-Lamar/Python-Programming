54772c149f8197e1324c14407a001416b0695046
Levi-Lamar <97467036+Levi-Lamar@users.noreply.github.com>
2022-01-28 16:46:42 +0300

    Add files via upload

---
+++ DataType Conversion.py
@@ -0,0 +1,10 @@
+#User input, input()
+#Type conversion
+age = int(input("Enter his age: "))
+age2 = float(input("Enter her age: "))
+
+print(age) 
+print(age2)
+
+print(type(age))
+print(type(age2))
---
+++ Datatypes.py
@@ -0,0 +1,9 @@
+#Datatypes
+a = 10
+b = 10.2
+c = "Hello Python"
+d = 2+3j
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
