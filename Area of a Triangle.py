22d61682f8064f30c4dc1dd2b000e8c2ac3173e5
Levi-Lamar <97467036+Levi-Lamar@users.noreply.github.com>
2022-01-28 16:45:36 +0300

    Add files via upload

---
+++ Area of a Triangle.py
@@ -0,0 +1,5 @@
+#Area of a Triangle
+b = float(input("Enter Base: "))
+h = float(input("Enter Height: "))
+Area = 1/2 * b * h
+print("Area of the Triangle =",Area)
---
+++ Concatenation.py
@@ -0,0 +1,7 @@
+#Concatenation
+a = "Hello"
+b = "Python"
+c = str(10)  #TypeError: can only concatenate str (not "int") to str
+print(a+b)
+print(a+c)
+print(a*5)
