878b199e26fd3019c6eb9a9ff0da4cde981b3a35
Levi-Lamar <97467036+Levi-Lamar@users.noreply.github.com>
2022-03-02 15:32:04 +0300

    Add files via upload

---
+++ accessing element.py
@@ -0,0 +1,10 @@
+# accessing a element from a Dictionary
+Dict = {1: 'Geeks', 'A':'Code', 'name': 'For'}
+ 
+# accessing a element using key
+print("Accessing a element using key:")
+print(Dict['A'])
+ 
+# accessing a element using key
+print("Accessing a element using key:")
+print(Dict[1])
