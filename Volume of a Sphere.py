5718da31e1961361806820666c27c3a08c066a18
Levi-Lamar <97467036+Levi-Lamar@users.noreply.github.com>
2022-03-16 09:32:00 +0300

    Add files via upload

---
+++ Volume of a Sphere.py
@@ -0,0 +1,5 @@
+#Volume of a Sphere
+r = float(input("Enter Radius: "))
+pi = 22/7
+Volume = 4/3 * pi * (r**3)
+print("Volume of a Sphere =",Volume)
---
+++ temperatures conversion to and from celsius_fahrenheit.py
@@ -0,0 +1,23 @@
+#Python program to convert temperatures to and from celsius, fahrenheit
+
+temp = input("Input the  temperature you like to convert? (e.g., 45F, 102C etc.) : ")
+
+degree = int(temp[:-1])
+
+i_convention = temp[-1]
+
+if i_convention.upper() == "C":
+
+  result = int(round((9 * degree) / 5 + 32))
+
+  print(temp, "is", result, "in Fahrenheit")
+
+elif i_convention.upper() == "F":
+
+  result = int(round((degree - 32) * 5 / 9))
+
+  print(temp, "is", result, "in Celsius")
+
+else:
+
+  print("Input proper convention.")
